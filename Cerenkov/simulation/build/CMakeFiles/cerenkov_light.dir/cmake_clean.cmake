5716681f509b9ce9e3c0d1068b9cf41f58e5718a
Pablo Vazquez <pablo.vazquez.cabaleiro@rai.usc.es>
2025-10-03 17:29:06 +0200

    First approximation to a simulation

---
+++ Cerenkov/simulation/build/CMakeFiles/cerenkov_light.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cerenkov_light.dir/sim.cc.o"
+  "CMakeFiles/cerenkov_light.dir/sim.cc.o.d"
+  "cerenkov_light"
+  "cerenkov_light.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cerenkov_light.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
